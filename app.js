9a137104b03a9339c7bfef8bcee305ba0aaa46a2
Heinrich Latreider <heinrich.latreider@haw-hamburg.de>
2017-02-25 16:56:42 +0100

    portfolio feature

--- app.js
+++ app.js
@@ -56,6 +56,12 @@ app.get("/about", (req, resp) => {
     });
 });
 
+app.get("/projects", (req, resp) => {
+    resp.render("projects.hbs", {
+        pageTitle: "My Projects",
+    });
+});
+
 app.get("/bad", (req, resp) => {
     resp.send({
         status: "404",
